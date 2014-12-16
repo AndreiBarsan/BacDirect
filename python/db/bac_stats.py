c3254f0671dd2c0d8a48289b750939774bc79d1d
Andrei Bârsan <andrei@siegedog.com>
2014-12-16 04:19:04 +0200

    Improved accuracy of contested result number cruncher

--- python/db/bac_stats.py
+++ python/db/bac_stats.py
@@ -14,13 +14,42 @@ from util import *
 
 # Returns how many students increased/lowered/didn't change their grade by
 # contesting the exam's result.  `op' is a mongoDB comparison operator.
-# TODO(ioan) The contested grade only gets taken into account if the difference
-# between it and the original grade is above a certain threshold, which depends
-# on either the original grade or the new one (it's 0.5 for smaller ones and 0.1
-# for bigger grades, but I'm not sure).  We should take that threshold into
-# account.
+# Contested results are only taken into consideration if they differ by 0.5 or
+# more points from the original grade, if the original grade is less than 9.5. 
+# If the original grade is greater than or equal to 9.5, then this threshold is
+# lowered to 0.1.
+# This function takes this into account.
 def compute_contested_grade_effect(subject, op):
 	contestedSubject = contestify(subject)
+	if op != '$eq':
+		matcher = {
+			"$match": {
+				"$and": [
+					{ 'comparePass': 1 },
+					{ '$or': [
+						
+						{ '$and': [{'original': { '$gte': 9.5 }}, { 'diff': { '$gte': 0.1 } }]},
+						{ '$and': [{'original': { '$lt': 9.5 }}, { 'diff': { '$gte': 0.5 } }]}
+					]}
+					
+				]
+			}
+		}
+	else:
+		# When looking for students unaffected by the updated exam results, we 
+		# need to include all students for whom the difference was too small.
+		matcher = {
+			"$match": {
+				"$and": [
+					{ '$or': [
+						{ '$and': [{'original': { '$gte': 9.5 }}, { 'diff': { '$lt': 0.1 } }]},
+						{ '$and': [{'original': { '$lt': 9.5 }}, { 'diff': { '$lt': 0.5 } }]}
+					]}
+					
+				]
+			}
+		}
+
 	res = fetch_table(get_db(), BAC_MONGO_TABLE).aggregate([
 		{
 			"$match": {
@@ -28,21 +57,28 @@ def compute_contested_grade_effect(subject, op):
 				contestedSubject: { '$gt': 1 }
 			}
 		},
-		# And who got a bigger grade as a result of that.
-		# This happens in two stages because Mongo.
+		# And who got a bigger/smaller grade as a result of that.
+		# This happens in two stages because of the way Mongo aggregations work.
 		{
 			"$project": {
-				contestedSubject: 1,
-				'eq': { "$cond": 
+				contestedSubject: True,
+				'comparePass': { "$cond": 
 					[ { op: [ '$' + contestedSubject, '$' + subject ] }, 1, 0 ]
-				}
+				},
+				'diff': { "$subtract": [ '$' + contestedSubject, '$' + subject ] },
+				'original': '$' + subject
 			}
 		},
 		{
-			"$match": {
-				'eq': 1
+			"$project": {
+				contestedSubject: True,
+				'comparePass': True,
+				# Compute absolute value of 'diff'.
+				'diff': { '$cond': [{'$lt': ['$diff', 0]}, {'$multiply': ['$diff', -1]}, '$diff'] },
+				'original': True
 			}
 		},
+		matcher,
 		{
 			"$group": {
 	   			"_id": None, 
